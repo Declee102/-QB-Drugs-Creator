a9ec549e06543e837d4acbb2a4146ab99e5e4beb
Declee102 <129567222+Declee102@users.noreply.github.com>
2025-06-27 13:48:05 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Declee102'
+description 'QBcore Drug Script with Built-in UI'
+version '1.0.0'
+
+shared_script '@qb-core/shared/locale.lua'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/app.js'
+}
